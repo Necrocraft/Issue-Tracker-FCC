c6a4e84e3b681296a869e4df0129737ff44d9d50
Glitch (zippy-uttermost-station) <none>
2020-07-18 10:30:49 +0000

    📷🌿 Checkpoint ./routes/api.js:9596039/2570

--- routes/api.js
+++ routes/api.js
@@ -51,16 +51,16 @@ module.exports = function (app) {
     const issueFound = issues.find(issue => issue._id === _id);
     if(!issueFound) {
        return res.json({
-          error: "No issue found with the given _id"
+          error: "No issue found with the given _id",
       })
     }
-    
-   issues = issues.map((issue) => issue._id === _id ? ({...issue, issue_title: issue_title === "" ? issue.issue_title : issue_title, issue_text: issue_text === "" ? issue.issue_text : issue_text, created_by: created_by === "" ? issue.created_by : created_by, assigned_to: assigned_to === "" ? issue.assigned_to : assigned_to, status_text: status_text === "" ? issue.status_text : status_text, open: open === "" ? issue.open : open, updated_on : new Date()}): issue);
-    
-    console.log(issueFound);
-    console.log(req.body);
-    console.log(issues);
-    res.test("");
+    if(issue_title === "" && issue_text === "" && created_by === "" && assigned_to === "" && status_text === "" && open === "") {
+      res.text("No updated field sent");
+    }
+    else {
+        issues = issues.map((issue) => issue._id === _id ? ({...issue, issue_title: issue_title === "" ? issue.issue_title : issue_title, issue_text: issue_text === "" ? issue.issue_text : issue_text, created_by: created_by === "" ? issue.created_by : created_by, assigned_to: assigned_to === "" ? issue.assigned_to : assigned_to, status_text: status_text === "" ? issue.status_text : status_text, open: open === "" ? issue.open : open, updated_on : new Date()}): issue);
+        res.test("Request Updated Sucessfully");
+      }
     })
     
     .delete(function (req, res){
