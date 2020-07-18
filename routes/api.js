1121fb1ea1daff0acb8929a7e7fa893d63bb9113
Glitch (zippy-uttermost-station) <none>
2020-07-18 13:45:53 +0000

    📕💠 Checkpoint ./routes/api.js:9596039/66

--- routes/api.js
+++ routes/api.js
@@ -49,17 +49,18 @@ module.exports = function (app) {
     var project = req.params.project;
     let {_id, issue_title, issue_text, created_by, assigned_to, status_text, open} = req.body;
     const issueFound = issues.find(issue => issue._id === _id);
+    console.log(req.body);
     if(!issueFound) {
        return res.json({
           error: "No issue found with the given _id",
       })
     }
-    if(issue_title === "" && issue_text === "" && created_by === "" && assigned_to === "" && status_text === "" && open === "") {
-      res.text("No updated field sent");
+    if(issue_title === "" && issue_text === "" && created_by === "" && assigned_to === "" && status_text === "") {
+      res.send("No updated field sent");
     }
     else {
         issues = issues.map((issue) => issue._id === _id ? ({...issue, issue_title: issue_title === "" ? issue.issue_title : issue_title, issue_text: issue_text === "" ? issue.issue_text : issue_text, created_by: created_by === "" ? issue.created_by : created_by, assigned_to: assigned_to === "" ? issue.assigned_to : assigned_to, status_text: status_text === "" ? issue.status_text : status_text, open: open === "" ? issue.open : open, updated_on : new Date()}): issue);
-        res.test("Request Updated Sucessfully");
+        res.send("Request Updated Sucessfully");
       }
     })
     
