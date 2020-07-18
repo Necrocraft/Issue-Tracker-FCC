ae9ffb5b8cf9fae671be78a63cc5e1e055f6cee3
Glitch (zippy-uttermost-station) <none>
2020-07-18 10:06:49 +0000

    👖🚀 Checkpoint ./routes/api.js:9596039/318

--- routes/api.js
+++ routes/api.js
@@ -55,11 +55,12 @@ module.exports = function (app) {
       })
     }
     
-    issues.map((issue) => issue._id === _id ? ({...issue, issue_title: issue_title === "" ? issue.issue_title : issue_title, issue_text: issue_text === "" ? issue.issue_text : issue_text, created_by: created_by === "" ? issue.created_by : created_by, assigned_to: assigned_to === "" ? issue.assigned_to : assigned_to, status_text, open}): issue);
+    issues.map((issue) => issue._id === _id ? ({...issue, issue_title: issue_title === "" ? issue.issue_title : issue_title, issue_text: issue_text === "" ? issue.issue_text : issue_text, created_by: created_by === "" ? issue.created_by : created_by, assigned_to: assigned_to === "" ? issue.assigned_to : assigned_to, status_text: status_text === "" ? issue.status_text : status_text, open: open === "" ? issue.open : open, updated_on : new Date()}): issue);
     
     console.log(issueFound);
     console.log(req.body);
-    res.json(issueFound);
+    console.log(issues);
+    res.json(issues.find(issue => issue._id === _id));
     })
     
     .delete(function (req, res){
